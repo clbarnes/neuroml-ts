51e7d2d84feb94a895873d480aaa8ba521b9f61b
Chris Barnes <barnesc@janelia.hhmi.org>
2019-10-17 18:15:45 -0400

    wip

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
+import { Parser } from "cxml";
+import * as neuroml2 from "./xmlns/www.neuroml.org/schema/neuroml2";
 export * from "./xmlns/www.neuroml.org/schema/neuroml2";
+
+
+export function parse(neuromlStr: string): neuroml2.document {
+    return Parser.parse(neuromlStr);
+}
